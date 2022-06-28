8a8e6e339cae65ee9ee98bcd463e9e08498e987e
syumai <syumai@gmail.com>
2022-06-28 13:07:29 +0900

    add bgcode option

--- cmd/syumaigen/main.go
+++ cmd/syumaigen/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"flag"
+	"image/color"
 	"image/gif"
 	"image/png"
 	"io"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/lucasb-eyer/go-colorful"
 	"github.com/syumai/syumaigen"
 )
 
 var (
 	scale     = flag.Int("scale", 10, "specify image scale")
 	code      = flag.String("code", "", "use color code")
-	norandom    = flag.Bool("norandom", false, "stop randomize color generation")
+	bgCode    = flag.String("bgcode", "", "use background color code")
+	norandom  = flag.Bool("norandom", false, "stop randomize color generation")
 	animated  = flag.Bool("animated", false, "generate animated GIF")
 	outputSVG = flag.Bool("svg", false, "generate SVG")
 )
@@ -38,6 +42,14 @@ func main() {
 		colorMap = syumaigen.GenerateColorMapByColorCode(*code)
 	} else if !*norandom {
 		colorMap = syumaigen.GenerateRandomColorMap()
+	}
+
+	if *bgCode != "" {
+		c, err := parseColorCode(*bgCode)
+		if err != nil {
+			log.Fatal(err)
+		}
+		colorMap[0] = c
 	}
 
 	if *outputSVG {
@@ -66,4 +78,16 @@ func main() {
 	if err := png.Encode(os.Stdout, img); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func parseColorCode(cc string) (color.Color, error) {
+	if !strings.HasPrefix(cc, "#") {
+		cc = "#" + cc
+	}
+	col, err := colorful.Hex(cc)
+	if err != nil {
+		return nil, err
+	}
+	h, c, l := col.Hcl()
+	return colorful.Hcl(h, c, l).Clamped(), nil
 }
